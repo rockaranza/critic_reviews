e76387e72e3d184f6f28a2a9913743a46b10d8a2
Diego Ocaranza <diego.ocaranza@outlook.com>
2023-12-29 12:11:30 -0500

    HTML structure and correction in the JavaScript array.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,36 @@ const reviews = [
   {
     autor: "James Berardinelli",
     critic: "Willy Wonka and the Chocolate Factory, like many childhood favorites, is better left in the past and allowed to reside in memories. As an adaptation of Dahl's story, it's inferior to the Burton remake.",
-    img: "../img/James Berardinelli.webp",
+    img: "./assets/img/James Berardinelli.webp",
   },
   {
     autor: "Roger Ebert",
     critic: "Probably the best film of its sort since The Wizard of Oz. It is everything that family movies usually claim to be, but aren't: Delightful, funny, scary, exciting, and, most of all, a genuine work of imagination.",
-    img: "../img/Roger Ebert.webp",
+    img: "./assets/img/Roger Ebert.webp",
   },
   {
     autor: "Joe Lipsett",
     critic: "A timeless classic with an all-time performance from Gene Wilder. This film is a marvel: weird, occasionally terrifying and visually wonderful.That chocolate lake looks disgusting, though.",
-    img: "../img/Joe Lipsett.webp",
+    img: "./assets/img/Joe Lipsett.webp",
   },
   {
     autor: "Betsy Bozdech",
     critic: "Director Mel Stuart [doesn't] shy away from bringing Dahl's unique, slightly twisted vision to the screen intact.",
-    img: "../img/Betsy Bozdech.webp",
+    img: "./assets/img/Betsy Bozdech.webp",
   },
   {
     autor: "Steven D. Greydanus",
     critic: "Aspires to the whimsy and fantasy of The Wizard of Oz but doesn't really capture the magic… more a fond tribute to 'pure imagination' than a triumph of it.",
-    img: "../img/Steven D Greydanus.webp",
+    img: "./assets/img/Steven D Greydanus.webp",
   },
   {
     autor: "Emanuel Levy",
     critic: "Starring Gene Wilder, this offbeat comedy-fantasy for children has achieved a status in pop culture",
-    img: "../img/Emanuel Levy.webp",
+    img: "./assets/img/Emanuel Levy.webp",
   },
   {
     autor: "Sean Axmaker",
     critic: "... that rare breed: an imaginative live-action kid's film that engages and delights adults.",
-    img: "../img/Sean Axmaker.webp",
+    img: "./assets/img/Sean Axmaker.webp",
   },
 ]
